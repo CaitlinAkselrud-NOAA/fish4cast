6bf87be62e359c421ce2be3ec1f82d2e825dcc9e
Caitlin Allen Akselrud NOAA <caitlin.allen_akselrud@noaa.gov>
2024-07-22 11:33:58 -0700

    build: starting to build input data and setup sections

--- code/main.R
+++ code/main.R
@@ -19,10 +19,29 @@ library(randomForest)
 functions <- list.files(here::here("functions"))
 purrr::walk(functions, ~ source(here::here("functions", .x)))
 
+file.create(here::here("output", "warnings.txt"), showWarnings = F)
+
 # specified parameters ----------------------------------------------------
 
 # any pre-specified or fixed values in the code should be set by the user
 #  in this section
+
+
+# # about input data ------------------------------------------------------
+
+in_ts <- T #T/F input is a time series
+
+
+# # random forest settings ------------------------------------------------
+
+# n trees
+
+# check
+
+# do you want to test every hyperparam combo? (computationally intensive)
+# TRUE means test every combo
+# FALSE mean test a max number of combos; default = 100.
+setup_hgrid <- F
 
 
 
@@ -31,9 +50,17 @@ purrr::walk(functions, ~ source(here::here("functions", .x)))
 
 # random forest -----------------------------------------------------------
 
-
 # # setup -----------------------------------------------------------------
 
+# auto-detect min_n, mtry
+
+# create grid
+if(setup_hgrid == T) {}
+if(setup_hgrid == F) {setup_gridmax = gridmax()}
+if(!is.logical(setup_hgrid)) {
+  print("ERROR: designate TRUE or FALSE for 'setup-hgrid' for a full or reduced hyperparamter grid size (respectively)")
+  # append to warnings.txt file
+  }
 
 # # training --------------------------------------------------------------
 
