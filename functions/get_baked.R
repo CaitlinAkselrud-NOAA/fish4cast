f749391922243e3b23dd8e55dba5956bd57ccd91
Caitlin Allen Akselrud NOAA <caitlin.allen_akselrud@noaa.gov>
2024-08-07 15:05:52 -0700

    build: function descriptions
    added roxygen skeleton for functions

--- functions/get_baked.R
+++ functions/get_baked.R
@@ -1,4 +1,14 @@
 
+#' get each k-fold
+#'
+#' @param k_dat data for k-folds
+#' @param splitnum which fold you're pulling
+#'
+#' @return list of each fold, structured with analysis and assessment data sections
+#' @export
+#'
+#' @examples
+#'
 get_k_breaks <- function(k_dat, splitnum)
 {
   analysis_data <- rsample::analysis(k_dat$splits[[splitnum]])
@@ -8,6 +18,15 @@ get_k_breaks <- function(k_dat, splitnum)
   return(list(analysis_data=analysis_data, assessment_data=assessment_data))
 }
 
+#' recipe for regression
+#'
+#' @param analysis_data analysis data for a specific fold
+#'
+#' @return recipe (aka formula) for each fold for RF regression
+#' @export
+#'
+#' @examples
+#'
 make_recipe <- function(analysis_data)
 {
   na_foo <- function(x,na_replace=-999)
@@ -23,6 +42,16 @@ make_recipe <- function(analysis_data)
   return(fish_recipe)
 }
 
+#' Input for RF analysis with each k-fold, analysis and assessment, and regression formula
+#'
+#' @param kfold input data
+#' @param splitN split number
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
 get_baked <- function(kfold, splitN)
 {
   d <- get_k_breaks(kfold, splitN)
@@ -49,6 +78,15 @@ get_baked <- function(kfold, splitN)
 
 }
 
+#' Testing data for RF predictions in the single fit case
+#'
+#' @param testing_data input testing data
+#'
+#' @return single data set with features and target
+#' @export
+#'
+#' @examples
+#'
 get_test_bake <- function(testing_data)
 {
   squid_recipe <- make_recipe(testing_data)
