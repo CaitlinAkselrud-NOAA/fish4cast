11d45f6cb90fed34503e7ff6285f94fb53637cf2
Caitlin Allen Akselrud NOAA <caitlin.allen_akselrud@noaa.gov>
2025-04-21 11:15:18 -0700

    build: test moving to wrapper fxns

--- code/main.R
+++ code/main.R
@@ -106,6 +106,8 @@ dists_feat<-lapply(all_features, function(x){x = "norm"})
 # how many states to test for in regime testing (max):
 max_states_test <- 2
 # CIA: currently error in clusters when >2 (test and add tryCatch to return NA when fails)
+# how many interations per HMM for regime testing:
+n_iters <- 200
 
 # * random forest settings ------------------------------------------------
 
@@ -232,48 +234,15 @@ future::plan(future::multicore, workers = n_workers)
 # CIA: add output for all user-specified settings for tracking projects
 
 # END OF USER SETTINGS SECTION --------------------------------------------
+
+# Insert wrapper functions here
+
 # -> THE FOLLOWING CODE IS AUTOMATED.
 
-# input data --------------------------------------------------------------
+all_input <- get_input_data(in_ts = in_ts, in_data = in_data, max_states_test = max_states_test,
+               all_features = all_features, dists_feat = dists_feat, n_iters = n_iters)
 
-# if your data is a time series, make sure it is arranged chronologically
-#   this is important later when you do your training/testing splits
-if(in_ts == TRUE){in_data <- in_data %>% arrange(time) }
-
-features <- in_data[which(names(in_data) != "target")]
-# features <- test_simple[which(names(test_simple) != "target")]
-# features_notime <- features[which(names(features) != "time")]
-
-# * input data exploration ------------------------------------------------
-# regime shift: assume discrete process (distinct shift)
-# based on Zoe Rand: HMMs
-
-regimes_aic <- vector(length = length(2:max_states_test))
-regimes_shift <- matrix(nrow = length(2:max_states_test),
-                        ncol = dim(all_features)[1])
-for(i in 2:max_states_test)
-{
-  # dat = just feature data;
-  # dist = dist type (eg normal) for each feature as a list;
-  # n-states-- here is where you can test multiple state settings, then later compare with AIC
-  # niters = number repeats within one hmm fit test
-  regimes <- get_regimes(dat = all_features,
-                           dat_dist = dists_feat,
-                           n_states = i, n_iters = 200)
-  # get_regimes already does model selection for the best fit given a specific n_state
-  regimes_aic[i-1] <- regimes$best_aic
-  regimes_shift[i-1, ] <- regimes$best_states
-}
-
-# this is model selection for the best number of states, if you are testing for more than one state (e.g. 2-5 states possible)
-aic_selex <- which.min(regimes_aic) # what is the best fit number of regimes, if testing for more than one
-fit_regime <- regimes_shift[aic_selex, ]
-regime_periods <- bind_cols(in_data, regime_detect = fit_regime) %>%
-  group_by(regime_detect) %>%
-  summarise(regime_start = min(time),
-            regime_end = max(time))
-
-reg_change <- which(diff(fit_regime) != 0) + 1
+all_input
 
 # shift detected 2007-2008; std recommended split btw 2013-2014
 
